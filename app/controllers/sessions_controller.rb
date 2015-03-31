48bfcbe65d732e421740a4eb1a5535a97a373258
Chandra Carney <chandracarney@me.com>
2015-03-30 21:25:11 -0600

    Privatizing auth_data in SessionsController.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
   def create
-    user = User.find_or_create_by_auth(request.env['omniauth.auth'])
+    user = User.find_or_create_by_auth(auth_data)
     session[:user_id] = user.id
     redirect_to user_path(current_user), notice: "Logged in as #{user.name}"
   end
@@ -8,5 +8,11 @@ class SessionsController < ApplicationController
   def destroy
     session.clear
     redirect_to root_path, notice: "You are logged out."
+  end
+
+  private
+
+  def auth_data
+    request.env["omniauth.auth"]
   end
 end
